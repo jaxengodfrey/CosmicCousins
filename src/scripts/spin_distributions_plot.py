fcae011a382cc76bdaf0d3e4c99293a6a3955032
Bruce Edelman <bruce.edelman@ligo.org>
2023-03-03 14:16:42 -0800

    Reduce memory overhead so github actions runner doesnt crash

--- src/scripts/spin_distributions_plot.py
+++ src/scripts/spin_distributions_plot.py
@@ -1,7 +1,7 @@
 import paths
 import numpy as np
 import matplotlib.pyplot as plt
-from utils import plot_mean_and_90CI, load_bsplinemass_ppd, plot_o3b_res, load_subpop_ppds
+from utils import plot_mean_and_90CI, load_subpop_ppds
 import deepdish as dd
 from matplotlib.ticker import ScalarFormatter
 
@@ -20,7 +20,6 @@ i_3 = 0,1
 subpop_ppds = load_subpop_ppds(g1 = True, g1_fname = 'bspline_1logpeak_100000s_ppds.h5')
 
 ax[i_0] = plot_mean_and_90CI(ax[i_0], subpop_ppds['a1'], subpop_ppds['peak_1_a1_pdfs']['unweighted'], color ='tab:cyan', label='Peak A', bounds = True, lw = 3, line_style = '--')
-# ax[i_0] = plot_mean_and_90CI(ax[i_0], subpop_ppds['a1'], subpop_ppds['peak_2_a1_pdfs'], color ='tab:purple', label='High-Mass Peak', bounds = True, lw = 3, line_style = '--')
 ax[i_0] = plot_mean_and_90CI(ax[i_0], subpop_ppds['a1'], subpop_ppds['continuum_a1_pdfs']['unweighted'], color ='tab:pink', label='Continuum B', bounds = True, lw = 3, line_style = (0, (1, 1)))
 ax[i_0].legend(frameon=False, fontsize=legend_text_size);
 ax[i_0].set_xlabel(r'$a$', fontsize=label_text_size)
@@ -33,9 +32,7 @@ ax[i_0].set_title('Base Model Spin Magnitude Distributions')
 ax[i_0].grid(True, which="major", ls=":")
 
 ax[i_1] = plot_mean_and_90CI(ax[i_1], subpop_ppds['cos_tilt_1'], subpop_ppds['peak_1_ct1_pdfs']['unweighted'], color ='tab:cyan', label='Peak A', bounds = True, lw = 3, line_style = '--')
-# ax[i_1] = plot_mean_and_90CI(ax[i_1], subpop_ppds['cos_tilt_1'], subpop_ppds['peak_2_tilt1_pdfs'], color ='tab:purple', label='High-Mass Peak', bounds = True, lw = 3, line_style = '--')
 ax[i_1] = plot_mean_and_90CI(ax[i_1], subpop_ppds['cos_tilt_1'], subpop_ppds['continuum_ct1_pdfs']['unweighted'], color ='tab:pink', label='Continuum B', bounds = True, lw = 3, line_style = (0, (1, 1)))
-# ax[i_1].legend(frameon=False, fontsize=legend_text_size);
 ax[i_1].set_xlabel(r'$cos(\theta)$', fontsize=label_text_size)
 ax[i_1].set_ylabel(r'$p(cos(\theta))$', fontsize=label_text_size)
 ax[i_1].grid(True, which="major", ls=":")
@@ -44,8 +41,6 @@ ax[i_1].set_ylim(0,1.2)
 ax[i_1].set_title('Base Model Tilt Distributions')
 ax[i_1].get_xaxis().set_major_formatter(ScalarFormatter())
 ax[i_1].grid(True, which="major", ls=":")
-
-from utils import plot_mean_and_90CI, load_iid_tilt_ppd, plot_o3b_spintilt, plot_o3b_spinmag, load_iid_mag_ppd
 
 subpop_ppds_1 = load_subpop_ppds(g2 = True, g2_fname = 'bspline_1logpeak_samespin_100000s_2chains.h5')#dd.io.load(paths.data / 'spin_popfrac_posteriors.h5')
 
@@ -56,7 +51,6 @@ ax[i_2].set_xlabel(r'$\cos{\theta}$', fontsize=label_text_size)
 ax[i_2].set_ylabel(r'$p(\cos{\theta})$', fontsize=label_text_size)
 ax[i_2].set_xlim(xmin, xmax)
 ax[i_2].set_title('Composite Model Tilt Distribution')
-# ax[i_2].legend(frameon=False, fontsize=legend_text_size, loc='upper left');
 ax[i_2].grid(True, which="major", ls=":")
 ax[i_2].set_ylim(0, 1.2)
 
@@ -75,5 +69,3 @@ ax[i_3].grid(True, which="major", ls=":")
 fig.tight_layout()
 plt.savefig(paths.figures / 'spin_distributions_plot.pdf', dpi=300);
 plt.savefig(paths.figures / 'spin_distributions_plot.jpeg', dpi=300);
-
-
